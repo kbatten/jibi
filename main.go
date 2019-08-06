108bb7458160801e7c9a22d70440ae81d731f9fa
Keith Batten <kbatten@gmail.com>
2019-08-06 06:56:24 -0700

    add LDHL SP, n

--- main.go
+++ main.go
@@ -14,15 +14,17 @@ func main() {
 	var config struct {
 		DevStatus     bool   `docopt:"--dev-status"`
 		DevMaxTicks   int    `docopt:"--dev-maxticks"`
+		DevLogInst    bool   `docopt:"--dev-loginstructions"`
 		DevCpuProfile bool   `docopt:"--dev-cpuprofile"`
 		Rom           string `docopt:"<rom>"`
 	}
 
 	usage := `usage: jibi [options] <rom>
 dev options:
-  --dev-status          show 1 second status
-  --dev-maxticks=TICKS  stop after a number of cpu ticks
-  --dev-cpuprofile      write cpu.prof for use with pprof`
+  --dev-status           show 1 second status
+  --dev-maxticks=TICKS   stop after a number of cpu ticks
+  --dev-loginstructions  write jibi.log for every instruction
+  --dev-cpuprofile       write cpu.prof for use with pprof`
 
 	opts, err := docopt.ParseDoc(usage)
 	if err != nil {
@@ -53,6 +55,7 @@ dev options:
 	options := jibi.Options{
 		Status:   config.DevStatus,
 		MaxTicks: config.DevMaxTicks,
+		LogInst:  config.DevLogInst,
 		Render:   true,
 		Keypad:   true,
 		Squash:   true,
